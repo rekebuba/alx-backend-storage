c26848b268f685b0c3466b313675fb3f45b7ea1e
rekebuba <abukiabdullahi1@gmail.com>
2024-08-09 00:20:24 +0300

    simple SQL query

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,5 +1,5 @@
--- Active: 1716468751032@@127.0.0.1@3306@holberton
-
+-- SQL script that ranks country origins of bands,
+-- ordered by the number of (non-unique) fans
 SELECT origin, SUM(fans) as nb_fans
 from metal_bands
 GROUP BY origin
