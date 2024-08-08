1e323d479e2c96e103a37fa26f44cfa85bc8c27e
rekebuba <abukiabdullahi1@gmail.com>
2024-08-09 00:16:14 +0300

    create users table

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -1,4 +1,4 @@
--- Active: 1716468751032@@127.0.0.1@3306@holberton
+-- SQL script that creates a table users
 CREATE TABLE IF NOT EXISTS users (
     id INT NOT NULL AUTO_INCREMENT,
     email VARCHAR(255) NOT NULL UNIQUE,
