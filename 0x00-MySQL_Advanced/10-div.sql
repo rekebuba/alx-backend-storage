4588645d7b159ffbd510531fd859399182390d60
rekebuba <abukiabdullahi1@gmail.com>
2024-08-09 00:45:00 +0300

    safe division

--- 0x00-MySQL_Advanced/10-div.sql
+++ 0x00-MySQL_Advanced/10-div.sql
@@ -1,10 +1,14 @@
--- Active: 1716468751032@@127.0.0.1@3306@holberton
+-- SQL script that creates a function SafeDiv that divides (and returns),
+-- the first by the second number or returns 0 if the second number is equal to 0.
 
+-- drop if exists
 DROP FUNCTION `SafeDiv`;
+
+-- change delimiter temporarily
 DELIMITER //
 
 CREATE FUNCTION SafeDiv(a INT, b INT) RETURNS FLOAT
-DETERMINISTIC
+DETERMINISTIC -- the output will always be the same given the same input
 BEGIN
     DECLARE result FLOAT;
 
@@ -17,4 +21,5 @@ BEGIN
     RETURN result;
 END //
 
+-- change back to default
 DELIMITER ;
