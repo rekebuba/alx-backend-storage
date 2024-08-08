d87760571b853d7bae04cbc69bf0a00042430307
rekebuba <abukiabdullahi1@gmail.com>
2024-08-09 00:54:58 +0300

    creates a view need_meeting

--- 0x00-MySQL_Advanced/11-need_meeting.sql
+++ 0x00-MySQL_Advanced/11-need_meeting.sql
@@ -1,5 +1,8 @@
--- Active: 1716468751032@@127.0.0.1@3306@holberton
-DROP VIEW need_meeting;
+-- SQL script that creates a view need_meeting,
+-- that lists all students that have a score under 80 (strict),
+-- and no last_meeting or more than 1 month.
+
+DROP VIEW IF EXISTS need_meeting;
 CREATE VIEW need_meeting AS
 SELECT name FROM students
 WHERE
