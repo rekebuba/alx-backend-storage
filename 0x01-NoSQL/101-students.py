c1ebcbed0422c2082ba495a22cc693cb08899570
rekebuba <abukiabdullahi1@gmail.com>
2024-08-14 02:35:25 +0300

    pycodestyle

--- 0x01-NoSQL/101-students.py
+++ 0x01-NoSQL/101-students.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 """sorted by average score"""
+
 list_all = __import__('8-all').list_all
+
 
 def top_students(mongo_collection):
     """function that returns all students sorted by average score
@@ -13,6 +15,7 @@ def top_students(mongo_collection):
     for student in students:
         scores = [topic.get('score') for topic in student.get('topics')]
         averageScore = sum(scores) / len(scores)
-        mongo_collection.update_one({'_id': student.get('_id')}, {'$set': { 'averageScore' : averageScore}})
+        mongo_collection.update_one({'_id': student.get('_id')}, {
+                                    '$set': {'averageScore': averageScore}})
 
-    return [result for result in mongo_collection.find().sort({ 'averageScore': -1 })]
+    return [x for x in mongo_collection.find().sort({'averageScore': -1})]
