253651cf0a21e09f279f88631ed04b3cdfbfdb64
rekebuba <abukiabdullahi1@gmail.com>
2024-08-14 23:45:21 +0300

    Writing strings to Redis

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,1 +1,26 @@
 #!/usr/bin/env python3
+"""Using Redis with python"""
+import redis
+import uuid
+
+
+class Cache:
+    def __init__(self) -> None:
+        """
+        Create a Redis client instance and store it as a private variable
+        """
+        self._redis = redis.Redis(host='localhost', port=6379, db=0)
+        self._redis.flushdb()
+
+    def store(self, data):
+        """generate a random key and store the input data in Redis
+
+        Args:
+            data (any): can be a str, bytes, int or float.
+
+        Returns:
+            _type_: return the random key
+        """
+        key = str(uuid.uuid4())
+        self._redis.set(key, data)
+        return key
